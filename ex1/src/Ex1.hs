4e7a13985d48f1e741a216e44b3594bcb6af942f
VictorWinberg <victor.m.winberg@gmail.com>
2018-03-29 17:04:11 +0200

    add hanoi and property tests

--- ex1/src/Ex1.hs
+++ ex1/src/Ex1.hs
@@ -2,6 +2,8 @@ module Ex1
     ( maxi
     , recsumsq
     , mapsumsq
+    , rechanoi
+    , maphanoi
     ) where
 
 maxi x y
@@ -11,11 +13,27 @@ maxi x y
 -- recursion
 recsumsq :: Integer -> Integer
 recsumsq 0 = 0
-recsumsq n = n * n + recsumsq (n - 1)
+recsumsq n
+  | n > 0 = n * n + recsumsq (n - 1)
+  | otherwise = -1
 
 -- map
 mapsumsq :: Integer -> Integer
-mapsumsq n = sum $ map sq [1..n]
-  where sq x = x * x
+mapsumsq n
+  | n >= 0 = sum $ map sq [1..n]
+  | otherwise = -1
+    where sq x = x * x
 
--- hanoi n
+-- recursion
+rechanoi :: Integer -> Integer
+rechanoi 1 = 1
+rechanoi n
+  | n > 1 = 2 ^ (n - 1) + rechanoi (n - 1)
+  | otherwise = -1
+
+-- map
+maphanoi :: Integer -> Integer
+maphanoi n
+  | n > 0 = sum $ map poweroftwo [0..n-1]
+  | otherwise = -1
+    where poweroftwo x = 2 ^ x
--- ex1/test/Spec.hs
+++ ex1/test/Spec.hs
@@ -15,6 +15,7 @@ recsumsqTests = testGroup "recsumsq tests"
   , testCase "one" $ recsumsq 1 @?= 1
   , testCase "two" $ recsumsq 2 @?= 5
   , testCase "three" $ recsumsq 3 @?= 14
+  , testCase "negative" $ recsumsq (-1) @?= -1
   ]
 
 mapsumsqTests = testGroup "mapsumsq tests"
@@ -22,12 +23,45 @@ mapsumsqTests = testGroup "mapsumsq tests"
   , testCase "one" $ mapsumsq 1 @?= 1
   , testCase "two" $ mapsumsq 2 @?= 5
   , testCase "three" $ mapsumsq 3 @?= 14
+  , testCase "negative" $ recsumsq (-1) @?= -1
   ]
+
+rechanoiTests = testGroup "rechanoi tests"
+  [ testCase "one" $ rechanoi 1 @?= 1
+  , testCase "two" $ rechanoi 2 @?= 3
+  , testCase "three" $ rechanoi 3 @?= 7
+  , testCase "four" $ rechanoi 4 @?= 15
+  , testCase "five" $ rechanoi 5 @?= 31
+  , testCase "negative" $ rechanoi (-1) @?= -1
+  ]
+
+maphanoiTests = testGroup "maphanoi tests"
+  [ testCase "one" $ maphanoi 1 @?= 1
+  , testCase "two" $ maphanoi 2 @?= 3
+  , testCase "three" $ maphanoi 3 @?= 7
+  , testCase "four" $ maphanoi 4 @?= 15
+  , testCase "five" $ maphanoi 5 @?= 31
+  , testCase "negative" $ maphanoi (-1) @?= -1
+  ]
+
+propertyTests = testGroup "property tests"
+  [ testProperty "recsumsq same as mapsumsq" sumsqRecSameAsMap
+  , testProperty "rechanoi same as maphanoi" hanoiRecSameAsMap
+  ]
+
+sumsqRecSameAsMap :: Integer -> Bool
+sumsqRecSameAsMap x = recsumsq x == mapsumsq x
+
+hanoiRecSameAsMap :: Integer -> Bool
+hanoiRecSameAsMap x = rechanoi x == maphanoi x
 
 allTests = testGroup "all tests"
   [ maxiTests
   , recsumsqTests
   , mapsumsqTests
+  , rechanoiTests
+  , maphanoiTests
+  , propertyTests
   ]
 
 main :: IO ()
