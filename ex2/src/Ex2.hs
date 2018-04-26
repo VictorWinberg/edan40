63093edbc98c20bd961df2ace771e5bce841a073
VictorWinberg <victor.m.winberg@gmail.com>
2018-04-26 16:18:45 +0200

    add [ex2] proposition, vars and truthValue

--- ex2/src/Ex2.hs
+++ ex2/src/Ex2.hs
@@ -1,6 +1,30 @@
 module Ex2
-    (
+    ( Proposition(..)
+    , vars
+    , truthValue
+    , deep
     ) where
 
 import Data.Maybe
 import Data.List
+
+data Proposition = VAR String
+  | AND Proposition Proposition
+  | OR Proposition Proposition
+  | NOT Proposition
+  deriving (Eq, Show)
+
+vars :: Proposition -> [String]
+vars (VAR p) = [p]
+vars (NOT p) = vars p
+vars (AND p q) = vars p ++ vars q
+vars (OR p q) = vars p ++ vars q
+
+truthValue :: Proposition -> [(String, Bool)] -> Bool
+truthValue (VAR p) = fromJust . lookup p
+truthValue (NOT p) = not . truthValue p
+truthValue (AND p q) = deep (&&) (truthValue p) (truthValue q)
+truthValue (OR p q) = deep (||) (truthValue p) (truthValue q)
+
+deep :: (Bool -> Bool -> Bool) -> (a -> Bool) -> (a -> Bool) -> a -> Bool
+deep op a b pq = op (a pq) (b pq)
--- ex2/test/Spec.hs
+++ ex2/test/Spec.hs
@@ -4,8 +4,18 @@ import Test.Tasty.QuickCheck
 
 import Ex2
 
+pq = [("p", True), ("q", False)]
+
+propositionTests = testGroup "proposition tests"
+  [ testCase "p" $ truthValue (VAR "p") pq @?= True
+  , testCase "!p" $ truthValue (NOT (VAR "p")) pq @?= False
+  , testCase "p && q" $ truthValue (AND (VAR "p") (VAR "q")) pq @?= False
+  , testCase "p && !q" $ truthValue (AND (VAR "p") (NOT (VAR "q"))) pq @?= True
+  , testCase "p || q" $ truthValue (OR (VAR "p") (VAR "q")) pq @?= True
+  ]
+
 allTests = testGroup "all tests"
-  [ 
+  [ propositionTests
   ]
 
 main :: IO ()
