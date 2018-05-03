1110e089ab0148e19fcc5a1529aff467cfbfd97e
VictorWinberg <victor.m.winberg@gmail.com>
2018-05-03 09:36:37 +0200

    add assignment 2 structure

---
+++ assign2/src/StringAlignment.hs
@@ -0,0 +1,1 @@
+module StringAlignment where
---
+++ assign2/test/Spec.hs
@@ -0,0 +1,11 @@
+import StringAlignment
+
+import Test.Tasty
+import Test.Tasty.HUnit
+
+allTests = testGroup "all tests"
+  [
+  ]
+
+main :: IO ()
+main = defaultMain allTests
