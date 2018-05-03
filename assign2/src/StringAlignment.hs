43ac6b16916a9eb7a2f9fcc22fc589e6b7d28437
easklund <dat14eas@student.lu.se>
2018-05-03 10:15:33 +0200

    explanation. ex. b

--- assign2/src/StringAlignment.hs
+++ assign2/src/StringAlignment.hs
@@ -2,3 +2,14 @@ module StringAlignment where
 
 similarityScore :: String -> String -> Int
 similarityScore a b = undefined
+
+
+{-attachHeads is adding h1 and h2 first in  each of the element in one duples.
+The funtion does this for all the duples in the list.
+h1 first in the first of the two duples and h2 firts in the second.
+Examlpe:
+attachHeads a e [(bcd, fgh), (jkl, nop)]
+gives us [(abcd, efgh), (ajkl, enop)]-}
+
+attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
